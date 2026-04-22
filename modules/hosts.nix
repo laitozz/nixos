c4471ee44bcfc432009b03ec72f0c800f5e2450b
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-04-22 22:18:16 +0300

    Clean up hosts file

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -2,12 +2,12 @@
 # then config their aspects in as many files you want
 {
 
-  den.hosts.x86_64-linux.fenix.users.lait = { };
-  den.hosts.x86_64-linux.onix.users.lait = { };
+  den.hosts.x86_64-linux.fenix.users.lait = { };        # laptop
+  den.hosts.x86_64-linux.onix.users.lait = { };         # desktop
   # den.hosts.x86_64-linux.nixie.users.lait = { };
-  # den.hosts.x86_64-linux.garnix.users.lait = { }; # for minimal setup?
-  # den.hosts.x86_64-linux.nocturnix.users.lait = { }; # for server?
-  # den.hosts.x86_64-linux.clinix.users.lait = { }; # harmonix, technix
+  # den.hosts.x86_64-linux.garnix.users.lait = { };     # for minimal setup?
+  # den.hosts.x86_64-linux.nocturnix.users.lait = { };  # for server?
+  # den.hosts.x86_64-linux.clinix.users.lait = { };     # harmonix, technix
 
   # define an standalone home-manager for tux
   # den.homes.x86_64-linux.tux = { };
