028ac22cf2c9843f58abaf6c9bdf3598868c0cc0
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-04-23 19:14:57 +0300

    Add fenix to syncthing

--- modules/base/syncthing.nix
+++ modules/base/syncthing.nix
@@ -11,9 +11,9 @@
         # TODO: set default user and password with sops-nix
         settings = {
           devices = {
-            "onix" = { id = "UEVQTKY-34ZQBJO-Z4DTM2K-M2QO5A5-UA5AUL4-3NQ5CPU-O2Y3HOL-DLWQKQO"; };
-            # "fenix" = { id = ""; };
-            "nord" = { id = "QNTINZC-OZDSEQM-P6LR362-5CELMLB-DTVI7KH-UDQPQGU-T4LMRQV-Q6TPGQZ"; };
+            "onix"  = { id = "UEVQTKY-34ZQBJO-Z4DTM2K-M2QO5A5-UA5AUL4-3NQ5CPU-O2Y3HOL-DLWQKQO"; };
+            "fenix" = { id = "Y4C57QV-HHB4XMP-6DAEPKM-W7P4ZXV-OWTUQKU-UYY4YV5-HUMHQPI-4M3XSQY"; };
+            "nord"  = { id = "QNTINZC-OZDSEQM-P6LR362-5CELMLB-DTVI7KH-UDQPQGU-T4LMRQV-Q6TPGQZ"; };
           };
           folders = {
             "Notes" = {
