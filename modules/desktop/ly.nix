f0914eb6721ad864c771927a2bcf86d098e1d044
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-04-24 23:35:33 +0300

    ly displaymanager settings (don't work)

--- modules/desktop/ly.nix
+++ modules/desktop/ly.nix
@@ -2,7 +2,14 @@
   sw.ly = {
     nixos = { pkgs, ... }: {
       # TODO: animation in github readme
-      services.displayManager.ly.enable = true;
+      services.displayManager.ly = {
+        enable = true;
+        # NOTE: Doesn't work with settings enabled
+        # settings = {
+        #   animate = true;
+        #   animation = "cmatrix";
+        # };
+      };
     };
   };
 }
