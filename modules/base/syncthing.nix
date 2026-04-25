3a37977d8b9b1edd4c930361f48000971988aae5
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-04-26 01:47:12 +0300

    Update syncthing Nord id

--- modules/base/syncthing.nix
+++ modules/base/syncthing.nix
@@ -11,18 +11,20 @@
         # TODO: set default user and password with sops-nix
         settings = {
           devices = {
-            "onix"  = { id = "UEVQTKY-34ZQBJO-Z4DTM2K-M2QO5A5-UA5AUL4-3NQ5CPU-O2Y3HOL-DLWQKQO"; };
-            "fenix" = { id = "Y4C57QV-HHB4XMP-6DAEPKM-W7P4ZXV-OWTUQKU-UYY4YV5-HUMHQPI-4M3XSQY"; };
-            "nord"  = { id = "QNTINZC-OZDSEQM-P6LR362-5CELMLB-DTVI7KH-UDQPQGU-T4LMRQV-Q6TPGQZ"; };
+            "Onix"  = { id = "UEVQTKY-34ZQBJO-Z4DTM2K-M2QO5A5-UA5AUL4-3NQ5CPU-O2Y3HOL-DLWQKQO"; };
+            "Fenix" = { id = "Y4C57QV-HHB4XMP-6DAEPKM-W7P4ZXV-OWTUQKU-UYY4YV5-HUMHQPI-4M3XSQY"; };
+            "Nord"  = { id = "XZZVVZT-MQ7ZGEH-HRWDQI6-SEPWSIS-WXOME3C-AA7WUSI-MINQ5YN-JXBTNAD"; };
           };
           folders = {
             "Notes" = {
               path = "~/notes";
-              devices = [ "onix" "fenix" "nord" ];
+              devices = [ "Onix" "Fenix" "Nord" ];
+              ignorePatterns = [ ".git" ];
             };
             "Org" = {
               path = "~/org";
-              devices = [ "onix" "fenix" "nord" ];
+              devices = [ "Onix" "Fenix" "Nord" ];
+              ignorePatterns = [ ".git" ];
             };
           };
         };
