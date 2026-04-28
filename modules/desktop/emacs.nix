95036440326339e9672a59a1daa28ab3c4c8cf0d
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-04-28 17:51:30 +0300

    Update doom install script to add doom bin to path

--- modules/desktop/emacs.nix
+++ modules/desktop/emacs.nix
@@ -13,7 +13,7 @@
       
       # Install script
       (pkgs.writeShellScriptBin "doomEmacsInstallScript" ''
-        emacsDir=~/.config/emacs
+        emacsDir=$HOME/.config/emacs
         if [ ! -d "$emacsDir" ] || [ ! -z "$(ls -A $emacsDir)" ]; then
           echo "Emacs configuration already exists."
           read -p "Remove it? (y/N): " continue
@@ -21,8 +21,10 @@
           echo "rm -rf $emacsDir"
           rm -rf $emacsDir
         fi
-        git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-        ~/.config/emacs/bin/doom install
+        git clone --depth 1 https://github.com/doomemacs/doomemacs $emacsDir
+        $emacsDir/bin/doom install
+        # Add directory to fish path
+        fish -c "fish_add_path $HOME/.config/emacs/bin"
       '')
     ];
   };
