8c9d25d9c7cf7610d543a00cffecfc3fffc8a127
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-04-29 12:59:35 +0300

    Clean up doom emacs install script

--- modules/desktop/emacs.nix
+++ modules/desktop/emacs.nix
@@ -1,5 +1,7 @@
 {
   sw.emacs.nixos = { pkgs, ... }: {
+    # NOTE: automatically starts emacs daemon
+    # TODO: Use exec-once emacs --daemon instead
     services.emacs.enable = true;
     # dependencies
     fonts.packages = with pkgs; [
@@ -21,10 +23,16 @@
           echo "rm -rf $emacsDir"
           rm -rf $emacsDir
         fi
+
         git clone --depth 1 https://github.com/doomemacs/doomemacs $emacsDir
         $emacsDir/bin/doom install
-        # Add directory to fish path
+
+        echo "Adding $emacsDir/bin to fish_user_paths"
         fish -c "fish_add_path $HOME/.config/emacs/bin"
+
+        echo "Doom emacs is now installed to $emacsDir"
+        echo "Make sure to manually remove all other emacs configurations"
+        echo "For example ~/.emacs.d"
       '')
     ];
   };
