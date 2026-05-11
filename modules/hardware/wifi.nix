6b4babf4b186ba09cefeb31d0198037f5e0fa462
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-05-11 13:47:50 +0300

    Add possibility of using iwd as NM backend

--- modules/hardware/wifi.nix
+++ modules/hardware/wifi.nix
@@ -3,6 +3,7 @@
     nixos = { pkgs, ... }: {
       networking.wireless.iwd.enable = true;
       # networking.networkmanager.enable = true;
+      # networking.networkmanager.wifi.backend = "iwd";
 
       environment.systemPackages = with pkgs; [
         impala
