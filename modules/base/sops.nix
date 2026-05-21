c394986a2fb52c16b412013b385f771a242b10c2
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-05-21 20:14:32 +0300

    Add secrets with sops

--- modules/base/sops.nix
+++ modules/base/sops.nix
@@ -14,5 +14,12 @@
       sops
       age
     ];
+    # TODO: Add desktop and confirm this works
+    sops.defaultSopsFile = ../../secrets/common.yaml;
+    sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+    sops.age.generateKey = true;
+    # Secrets
+    sops.secrets."wireguard/home-router" = { };
   };
 }
