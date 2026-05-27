83cf77d24eb924895f6bd67ec1effd5cfd284814
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-05-27 22:09:33 +0300

    Disable wireguard by default

--- modules/services/wireguard.nix
+++ modules/services/wireguard.nix
@@ -1,9 +1,8 @@
 {
-  # Wireguard needs to be stopped with systemd manually
-  # sudo systemctl stop wg-quick-wg0.service
   sw.wireguard.nixos = { pkgs, config, ... }: {
     environment.systemPackages = with pkgs; [ wireguard-tools ];
     networking.wg-quick.interfaces.wg0 = {
+      autostart = false;
       address = [ "10.6.0.3/32" ];
       dns = [ "10.6.0.1" ];
       privateKeyFile = config.sops.secrets."wireguard/home-router".path;
@@ -13,6 +12,11 @@
         publicKey = "nV4DqSBWhYa2EP+NhR6Vt85VsQGKZ1WAVPwx9lxGETs=";
         persistentKeepalive = 25;
       }];
+    };
+    environment.shellAliases = {
+      wg0up = "sudo systemctl start wg-quick-wg0.service";
+      wg0down = "sudo systemctl stop wg-quick-wg0.service";
+      wg0status = "systemctl status wg-quick-wg0.service";
     };
   };
 }
